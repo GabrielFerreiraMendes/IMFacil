5778d34e3faa72267829cae744b255d1eb5e6cd9
Gabriel Ferreira Mendes <gabriel.mendes@sankhya.com.br>
2024-09-20 20:53:58 -0300

    simplificando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AngularMaterialModule } from './shared/angular-material.module';
-import { Pessoa } from './model/pessoa';
 import { FormsModule } from '@angular/forms';
-import { elementAt } from 'rxjs';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTableModule } from '@angular/material/table';
 
 export interface tabelaImc {
   imc: string;
@@ -12,20 +14,34 @@ export interface tabelaImc {
 }
 
 const ELEMENT_DATA: tabelaImc[] = [
-  {imc: 'Menor que 18,5', classificacao: 'Magreza', obesidade: '0'},
-  {imc: 'Entre 18,5 e 24,9', classificacao: 'Normal', obesidade: '0'},
-  {imc: 'Entre 25,0 e 29,9', classificacao: 'Sobrepeso', obesidade: '1'},
-  {imc: 'Entre 30,0 e 39,9', classificacao: 'Obesidade', obesidade: '2'},
-  {imc: 'Maior que 40,0', classificacao: 'Obesidade Grave', obesidade: '3'},
+  { imc: 'Menor que 18,5', classificacao: 'Magreza', obesidade: '0' },
+  { imc: 'Entre 18,5 e 24,9', classificacao: 'Normal', obesidade: '0' },
+  { imc: 'Entre 25,0 e 29,9', classificacao: 'Sobrepeso', obesidade: '1' },
+  { imc: 'Entre 30,0 e 39,9', classificacao: 'Obesidade', obesidade: '2' },
+  { imc: 'Maior que 40,0', classificacao: 'Obesidade Grave', obesidade: '3' },
 ];
+
+class Pessoa {
+  altura: string = '';
+  peso: string = '';
+  imc: string = '';
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, AngularMaterialModule],
+  imports: [
+    RouterOutlet,
+    FormsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatTableModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
   title = 'imc_pj';
 
@@ -35,8 +51,8 @@ export class AppComponent {
   displayedColumns: string[] = ['imc', 'classificacao', 'obesidade'];
   dataSource: any[] = ELEMENT_DATA;
 
-  calcularImc(pessoa : Pessoa): void {
-  this.clickedRows.clear();
+  calcularImc(pessoa: Pessoa): void {
+    this.clickedRows.clear();
 
     const altura = parseFloat(pessoa.altura.replace(',', '.'));
     const peso = parseFloat(pessoa.peso.replace(',', '.'));
@@ -47,23 +63,23 @@ export class AppComponent {
       pessoa.imc = '';
     }
 
-    if (parseFloat(pessoa.imc) <= 18.5){
+    if (parseFloat(pessoa.imc) <= 18.5) {
       this.clickedRows.add(ELEMENT_DATA[0]);
     }
 
-    if(parseFloat(pessoa.imc) > 18.5 && parseFloat(pessoa.imc) <= 24.9){
+    if (parseFloat(pessoa.imc) > 18.5 && parseFloat(pessoa.imc) <= 24.9) {
       this.clickedRows.add(ELEMENT_DATA[1]);
     }
 
-    if(parseFloat(pessoa.imc) >= 25 && parseFloat(pessoa.imc) <= 29.9){
+    if (parseFloat(pessoa.imc) >= 25 && parseFloat(pessoa.imc) <= 29.9) {
       this.clickedRows.add(ELEMENT_DATA[2]);
     }
 
-    if(parseFloat(pessoa.imc) >= 30 && parseFloat(pessoa.imc) <= 39.9){
+    if (parseFloat(pessoa.imc) >= 30 && parseFloat(pessoa.imc) <= 39.9) {
       this.clickedRows.add(ELEMENT_DATA[3]);
     }
 
-    if(parseFloat(pessoa.imc) >= 40 ){
+    if (parseFloat(pessoa.imc) >= 40) {
       this.clickedRows.add(ELEMENT_DATA[4]);
     }
   }
--- src/app/model/pessoa.ts
+++
@@ -1,5 +0,0 @@
-export class Pessoa {
-  altura: string = '';
-  peso: string = '';
-  imc: string = '';
-}
